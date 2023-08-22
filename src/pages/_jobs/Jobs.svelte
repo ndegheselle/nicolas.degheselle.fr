c711e7210145a01e7a37bf3a53059293ae0b7276
Nicolas de gheselle <nicolas@degheselle.fr>
2023-08-22 18:46:02 +0200

    Added job description

--- src/pages/_jobs/Jobs.svelte
+++ src/pages/_jobs/Jobs.svelte
@@ -1,7 +1,10 @@
 <script>
-    function getMonthsBetweenDates(startDate, endDate) {
+    function getMonthsBetweenDates(_jobs) {
+        const startDate = new Date(_jobs[0].startingDate);
+        const endDate = new Date();
+
         let currentJobIndex = 0;
-        let months = [];
+        let lines = [];
         let currentDate = new Date(startDate);
         endDate.setDate(1);
 
@@ -10,50 +13,118 @@
             const month = currentDate.getMonth() + 1; // Months are 0-based, so add 1
             const line = { year, month };
 
-
             const job = jobs[currentJobIndex];
             job.startingDate = new Date(job.startingDate).getTime();
-            job.endingDate = new Date(job.endingDate).getTime() || new Date().getTime();
+            job.endingDate =
+                new Date(job.endingDate).getTime() || new Date().getTime();
 
-            if (currentDate.getTime() > job.endingDate && currentJobIndex < jobs.length - 1)
+            if (
+                currentDate.getTime() > job.endingDate &&
+                currentJobIndex < jobs.length - 1
+            )
                 currentJobIndex++;
-            
-            if (currentDate.getTime() <= job.endingDate && currentDate.getTime() >= job.startingDate)
-            {
+
+            if (
+                currentDate.getTime() <= job.endingDate &&
+                currentDate.getTime() >= job.startingDate
+            ) {
+                line.job = job;
                 line.isIntenship = job.isInternship;
                 line.isJob = !job.isInternship;
-                if (months[months.length - 1])
-                    line.isFirstOrLast = !months[months.length - 1].isJob;
+                if (lines[lines.length - 1])
+                    line.isFirstOrLast = !lines[lines.length - 1].isJob;
             }
-            
-            if (!line.isJob && months[months.length - 1]?.isJob)
-                months[months.length - 1].isFirstOrLast = true;
+
+            if (!line.isJob && lines[lines.length - 1]?.isJob)
+                lines[lines.length - 1].isFirstOrLast = true;
 
             // Move to the next month
             currentDate.setMonth(currentDate.getMonth() + 1);
-            months.push(line);
+            lines.push(line);
         }
 
-        if (months[months.length - 1].isJob)
-            months[months.length - 1].isFirstOrLast = true;
+        if (lines[lines.length - 1].isJob)
+            lines[lines.length - 1].isFirstOrLast = true;
 
-        return months;
+        console.log("paujreazo");
+        return lines;
     }
 
+    function selectJob(_job) {
+        if (_job == null) return;
+        selectedJob = _job;
+    }
+    function formatDate(timestamp)
+    {
+        return new Date(timestamp).toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+        });
+    }
+
+    let selectedJob = null;
+    $: months = getMonthsBetweenDates(jobs);
     export let jobs = [];
 </script>
 
-<div class="grid">
-    {#each getMonthsBetweenDates(new Date(jobs[0].startingDate), new Date()) as month}
-        <span class="timeline-bar" class:is-internship={month.isIntenship} class:is-job={month.isJob} class:first-or-last={month.isFirstOrLast}/>
+<div class="grid timeline">
+    {#each months as month}
+        <a
+            class="timeline-container"
+            on:mouseenter={() => selectJob(month.job)}
+            on:click={() => selectJob(month.job)}
+            on:keydown={() => selectJob(month.job)}
+            on:keyup={() => selectJob(month.job)}
+            on:keypress={() => selectJob(month.job)}
+        >
+            <span
+                class="timeline-bar"
+                class:is-internship={month.isIntenship}
+                class:is-job={month.isJob}
+                class:first-or-last={month.isFirstOrLast}
+            />
+            {#if month.month == 1}
+                <span class="year">{month.year}</span>
+            {/if}
+        </a>
     {/each}
 </div>
 
+<div class="grid">
+    <div class="col is-6">
+        <div class="card" class:active={selectedJob}>
+            {#if selectedJob}
+                <h3>{selectedJob.company}</h3>
+                <p class="date">
+                    {formatDate(selectedJob.startingDate)} -
+                    {formatDate(selectedJob.endingDate)}
+                </p>
+                <p>{selectedJob.description}</p>
+            {/if}
+        </div>
+    </div>
+</div>
+
 <style>
-    .timeline-bar {
+    .timeline {
+        flex-wrap: nowrap;
+        overflow-x: auto;
+    }
+
+    .timeline-container {
         transition: 0.3s;
+        position: relative;
+        padding: 0 0.1rem;
+    }
+    .timeline-container .year {
+        font-family: "Teko", sans-serif;
+        position: absolute;
+        color: var(--color-front-secondary);
+    }
+
+    .timeline-bar {
         display: block;
-        margin: 0 0.1rem;
+        margin: 0.5rem 0;
         width: 0.3rem;
         height: 1rem;
         border-radius: 0.4rem;
@@ -62,15 +133,32 @@
     .timeline-bar.is-internship {
         background-color: var(--color-primary-dim);
         height: 1.5rem;
+        margin: 0.25rem 0;
     }
     .timeline-bar.is-job {
         background-color: var(--color-primary);
         height: 2rem;
+        margin: 0;
     }
     .timeline-bar.first-or-last {
         height: 1.5rem;
     }
-    .timeline-bar:hover {
+    .timeline-container:hover {
         transform: scaleY(1.2);
+    }
+
+    .card {
+        opacity: 0;
+        transition: 1s;
+        margin-top: 2rem;
+        overflow: hidden;
+        max-height: 0;
+    }
+    .card.active {
+        opacity: 1;
+        max-height: 500px;
+    }
+    .card .date {
+        color: var(--color-front-secondary);
     }
 </style>
