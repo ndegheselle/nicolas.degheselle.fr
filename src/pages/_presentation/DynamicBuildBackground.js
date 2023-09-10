c69d578c6b4d59d3cb60b26060f3134f064b4961
Nicolas de gheselle <nicolas@degheselle.fr>
2023-09-10 22:30:36 +0200

    Improve coding animation

--- src/pages/_presentation/DynamicBuildBackground.js
+++ src/pages/_presentation/DynamicBuildBackground.js
@@ -1,6 +1,6 @@
 let currentTarget = null;
 
-function resizePreviewOn(target) {
+function resizePreviewLineOn(target) {
   if (!target) return;
   
   const buildPreviewElement = document.getElementById("buildPreview");
@@ -12,6 +12,31 @@ function resizePreviewOn(target) {
   widthElement.style.left = `${targetRect.left - 1}px`;
   heightElement.style.height = `${targetRect.height + 1}px`;
   heightElement.style.top = `${targetRect.top - 1}px`;
+}
+
+function addCssCodingToBackground(line) {
+  const container = document.getElementById("codingCss");
+  const span = document.createElement("span");
+
+  if (line.includes(";")) {
+    span.classList.add("ml-4");
+  }
+  span.innerHTML = line;
+  container.append(span);
+}
+
+function updateBackground(timeStamp) {
+  if(!currentTarget) return;
+
+  resizePreviewLineOn(currentTarget);
+  window.requestAnimationFrame(updateBackground);
+}
+
+function setupBackgroundPreviewLine(elementName) {
+  const element = document.querySelector(elementName);
+  if (!element) return;
+  currentTarget = element;
+  window.requestAnimationFrame(updateBackground);
 }
 
 function hidePreview() {
@@ -33,46 +58,23 @@ function buildStyle(idCssToBuild, idCssContainer) {
 
   const newCss = templateHTML.slice(0, actualCss + 1);
   const line = newCss[newCss.length - 1];
-  addCssCoding(line);
 
   if (line.includes("{")) {
     const elementName = line.replace("}", "").replace("{", "").trim();
-    setupBackgroundPreview(elementName);
+    setupBackgroundPreviewLine(elementName);
   }
+
+  addCssCodingToBackground(line);
 
   container.innerHTML = newCss.join("\n") + "}";
   return true;
 }
 
-function setupBackgroundPreview(elementName) {
-  const element = document.querySelector(elementName);
-  if (!element) return;
-  resizePreviewOn(element);
-}
-
-function addCssCoding(line) {
-  const container = document.getElementById("codingCss");
-  const span = document.createElement("span");
-
-  if (line.includes(";")) {
-    span.classList.add("ml-4");
-  }
-  span.innerHTML = line;
-  container.append(span);
-}
-
 export function animateBuild(idCssToBuild, idCssContainer) {
-
-  function resizeOnCurrent() {
-    resizePreviewOn(currentTarget);
-  }
-  window.addEventListener('resize', resizeOnCurrent);
   var intervalId = window.setInterval(function () {
     const haveBuildRemaining = buildStyle(idCssToBuild, idCssContainer);
     if (!haveBuildRemaining) {
       clearInterval(intervalId);
-      
-      window.removeEventListener('resize', resizeOnCurrent);
       hidePreview();
     }
   }, 500);
