edde6c5b8f85b1a32fc33d506690831d46b4c1f2
Nicolas de gheselle <nicolas@degheselle.fr>
2024-02-26 23:12:09 +0100

    Add book style and animation

---
+++ src/pages/_knowledges/Bookshelf.svelte
@@ -0,0 +1,135 @@
+<script>
+    import { SVG } from "@svgdotjs/svg.js";
+    import { onMount, createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function bookSelected() {
+        dispatch("bookSelected", { book: "book" });
+    }
+    function bookHovered() {
+        dispatch("bookHovered", { book: "book" });
+    }
+
+    function randomIntFromInterval(min, max) {
+        // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    onMount(() => {
+        draw = SVG(svg)
+            .size("100%", "100%")
+            .viewbox(
+                0,
+                0,
+                BOOKSHELF_WIDTH,
+                BOOKSHELF_HEIGHT,
+            );
+
+        draw.rect(BOOKSHELF_WIDTH, SUPPORT_SIZE).fill("cyan");
+        draw.rect(BOOKSHELF_WIDTH, SUPPORT_SIZE)
+            .move(0, BOOKSHELF_HEIGHT - SUPPORT_SIZE)
+            .fill("cyan");
+
+        // Last vertical support
+        draw.rect(SUPPORT_SIZE, BOOKSHELF_HEIGHT)
+            .move(BOOKSHELF_WIDTH - SUPPORT_SIZE * 2, 0)
+            .fill("magenta");
+
+        createCols(draw);
+    });
+
+    function createCols(parent) {
+        const colWidth = BOOKSHELF_WIDTH / bookshelf.length;
+
+        for (let i = 0; i < bookshelf.length; i++) {
+            const col = bookshelf[i];
+            const colGroup = parent.group().translate(i * colWidth, 0);
+            colGroup
+                .text(col.tag)
+                .x(colWidth / 2)
+                .y(1)
+                .attr("text-anchor", "middle");
+
+            // Horizontal supports
+            colGroup
+                .rect(SUPPORT_SIZE, BOOKSHELF_HEIGHT)
+                .move(SUPPORT_SIZE, 0)
+                .fill("magenta");
+
+            createBooks(col, colGroup);
+        }
+    }
+
+    function createBooks(col, parent) {
+        let currentBookNumber = 0;
+
+        for (let i = 0; i < col.books.length; i++) {
+            const book = col.books[i];
+            const bookGroup = parent
+                .group()
+                .translate(BOOK_WIDTH * currentBookNumber + BOOK_GROUP_SPACING * i, 0)
+                .addClass("book-group");
+
+            bookGroup
+                .text(book.title)
+                .x((book.volumes.length * BOOK_WIDTH) / 2)
+                .y(20)
+                .attr("text-anchor", "middle");
+            book.volumes.forEach((volume, j) => {
+                // Random number between 0.8 and 1.2
+                const randHeight = Math.random() * 0.2 + 0.9;
+                const randBook = randomIntFromInterval(1, 4);
+
+                let book = bookGroup
+                    // .rect(BOOK_WIDTH, BOOK_HEIGHT + randomHeight)
+                    .use('book-' + randBook)
+                    .move(
+                        BOOK_WIDTH * j + SUPPORT_SIZE * 2,
+                        BOOKSHELF_HEIGHT -
+                            BOOK_HEIGHT -
+                            SUPPORT_SIZE,
+                    )
+                    .scale(1, randHeight, 0, BOOKSHELF_HEIGHT - SUPPORT_SIZE);
+
+                book.on("mouseenter", () => {
+                    bookGroup.addClass("is-active");
+                }).on("mouseleave", () => {
+                    bookGroup.removeClass("is-active");
+                });
+            });
+            bookGroup
+                .rect(
+                    BOOK_WIDTH * book.volumes.length,
+                    BOOK_HEIGHT + BOOK_RANDOM_HEIGHT,
+                )
+                .move(
+                    SUPPORT_SIZE * 2,
+                    BOOKSHELF_HEIGHT -
+                        BOOK_HEIGHT -
+                        BOOK_RANDOM_HEIGHT -
+                        SUPPORT_SIZE,
+                )
+                .addClass("book-group-selection");
+
+            currentBookNumber += book.volumes.length;
+        }
+    }
+
+    const BOOKSHELF_WIDTH = 500;
+    const BOOKSHELF_HEIGHT = 200;
+    const SUPPORT_SIZE = 8;
+
+    const BOOK_WIDTH = 20;
+    const BOOK_HEIGHT = 100;
+    const BOOK_RANDOM_HEIGHT = 10;
+    const BOOK_GROUP_SPACING = 2;
+
+    let draw;
+    let svg;
+    export let bookshelf = [];
+</script>
+
+<svg bind:this={svg}>
+    <use xlink:href="#bookshelf-defs" />
+</svg>
---
+++ src/pages/_knowledges/Library.svelte
@@ -0,0 +1,135 @@
+<script>
+    import Bookshelf from "./Bookshelf.svelte";
+
+    let bookcase = [
+        [
+            {
+                tag: "Languages",
+                books: [
+                    {
+                        title: "Javascript",
+                        description: "101",
+                        volumes: ["101", "Promises", "Everything else"],
+                    },
+                    {
+                        title: "Javascript",
+                        description: "Promises",
+                        volumes: ["101"],
+                    },
+                ],
+            },
+            {
+                tag: "Other",
+                books: [
+                    {
+                        title: "aa",
+                        description: "The most popular language",
+                        volumes: ["101", "Promises", "Everything else"],
+                    },
+                ],
+            },
+        ],
+    ];
+
+</script>
+
+<svg display="none"
+    viewBox="0 0 20 100">
+
+    <style>
+        .color-1 {
+            fill: #555;
+        }
+
+        .color-2 {
+            fill: #777;
+        }
+
+        .color-3 {
+            fill: #ddd;
+        }
+        .color-background {
+            fill: var(--color-bg);
+        }
+    </style>
+
+    <defs>
+        <g id="book-top" >
+            <rect width="20" height="26" class="color-2" rx="4"/>
+            <rect width="14" height="22" x="3" class="color-3" />
+            <rect width="14" height="3" x="3" y="-1" class="color-background" />
+        </g>
+
+        <g id="book-1" class="book">
+            <g class="front" transform="translate(0,20)">
+                <rect width="20" height="80" class="color-1" rx="4"/>
+                <rect width="10" height="60" x="5" y="10" class="color-2" />
+            </g>
+            <use href="#book-top" class="top" transform-origin="10 22" />
+        </g>
+
+        <g id="book-2" class="book">
+            <g class="front" transform="translate(0,20)">
+                <rect width="20" height="80" class="color-1" rx="4"/>
+                <rect width="16" height="10" x="2" y="10" class="color-2" />
+                <rect width="16" height="10" x="2" y="60" class="color-2" />
+            </g>
+            <use href="#book-top" class="top" transform-origin="10 22"/>
+        </g>
+
+        <g id="book-3" class="book">
+            <g class="front" transform="translate(0,20)">
+                <rect width="20" height="80" class="color-1" rx="4"/>
+                <rect width="12" height="10" x="4" y="15" class="color-2" />
+                <rect width="12" height="10" x="4" y="30" class="color-2" />
+                <rect width="12" height="10" x="4" y="45" class="color-2" />
+            </g>
+            <use href="#book-top" class="top" transform-origin="10 22"/>
+        </g>
+
+        <g id="book-4" class="book">
+            <g class="front" transform="translate(0,20)">
+                <rect width="20" height="80" class="color-1" rx="4"/>
+                <rect width="4" height="45" x="5" y="12" class="color-2" />
+                <rect width="4" height="35" x="10" y="22" class="color-2" />
+                <rect width="12" height="10" x="4" y="60" class="color-2" />
+            </g>
+            <use href="#book-top" class="top" transform-origin="10 22"/>
+        </g>
+    </defs>
+
+    <use href="#book-2" />
+</svg>
+
+{#each bookcase as bookshelf}
+    <div>
+        <Bookshelf {bookshelf} />
+    </div>
+{/each}
+
+<style>
+    :global(.book-group-selection) {
+        stroke-width: 1;
+        stroke-dasharray: 5;
+        fill: none;
+    }
+    :global(.book-group.is-active .book-group-selection) {
+        stroke: red;
+    }
+    .book .top {
+        transition: 0.3s;
+        transform: scaleY(0);
+    }
+    .book {
+        transition: 0.3s;
+    }
+
+
+    .book:hover .top {
+        transform: scaleY(1);
+    }
+    .book:hover {
+        transform: translateY(10px);
+    }
+
+</style>
