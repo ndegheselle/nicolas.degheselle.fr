440d66d15e7b6c9ebb5838021480980d7b8733b8
Nicolas de gheselle <nicolas@degheselle.fr>
2024-03-02 22:55:43 +0100

    Update LibraryStore.js

--- src/pages/_knowledges/LibraryStore.js
+++ src/pages/_knowledges/LibraryStore.js
@@ -2,3 +2,7 @@ import { writable } from 'svelte/store';
 
 export const activeBook = writable(null);
 export const isBookSelected = writable(false);
+
+// set active
+// set previous as inactive
+// get element and set classes
