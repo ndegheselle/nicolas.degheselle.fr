46817bddaf9fde80e158735b31eab46966062d74
Nicolas de gheselle <nicolas@degheselle.fr>
2024-03-12 20:09:11 +0100

    Passed jobs to svg

--- src/pages/_jobs/Jobs.svelte
+++ src/pages/_jobs/Jobs.svelte
@@ -1,56 +1,7 @@
 <script>
     import Card from "../../components/Card.svelte";
-
-    function getMonthsBetweenDates(_jobs) {
-        const startDate = new Date(_jobs[0].startingDate);
-        const endDate = new Date();
-
-        let currentJobIndex = 0;
-        let lines = [];
-        let currentDate = new Date(startDate);
-        endDate.setDate(1);
-
-        while (currentDate <= endDate) {
-            const year = currentDate.getFullYear();
-            const month = currentDate.getMonth() + 1; // Months are 0-based, so add 1
-            const line = { year, month };
-
-            const job = jobs[currentJobIndex];
-            job.startingDate = new Date(job.startingDate).getTime();
-            job.endingDate =
-                new Date(job.endingDate).getTime() || new Date().getTime();
-
-            if (
-                currentDate.getTime() > job.endingDate &&
-                currentJobIndex < jobs.length - 1
-            )
-                currentJobIndex++;
-
-            if (
-                currentDate.getTime() <= job.endingDate &&
-                currentDate.getTime() >= job.startingDate
-            ) {
-                line.job = job;
-                line.isIntenship = job.isInternship;
-                line.isJob = !job.isInternship;
-                if (lines[lines.length - 1])
-                    line.isFirstOrLast = !lines[lines.length - 1].isJob;
-            }
-
-            if (!line.isJob && lines[lines.length - 1]?.isJob)
-                lines[lines.length - 1].isFirstOrLast = true;
-
-            // Move to the next month
-            currentDate.setMonth(currentDate.getMonth() + 1);
-            lines.push(line);
-        }
-
-        if (lines[lines.length - 1].isJob)
-            lines[lines.length - 1].isFirstOrLast = true;
-
-        selectJob(_jobs[_jobs.length - 1]);
-        return lines;
-    }
+    import { SVG } from "@svgdotjs/svg.js";
+    import { onMount } from "svelte";
 
     function selectJob(_job) {
         if (_job == null) return;
@@ -61,45 +12,73 @@
         return new Date(timestamp).toLocaleDateString(undefined, {
             year: "numeric",
             month: "long",
-        });
+        }).replace(/^\w/, c => c.toUpperCase());
     }
 
-    let selectedJob = null;
-    $: months = getMonthsBetweenDates(jobs);
+    onMount(() => {
+        const draw = SVG(svg).viewbox(0, 0, WIDTH, HEIGHT + EXTERIOR_SPACING * 2);
+
+        let totalDuration = 0;
+        for (let i = 0; i < jobs.length; i++) {
+            const job = jobs[i];
+            job.startingDate = new Date(job.startingDate).getTime();
+            job.endingDate =
+                new Date(job.endingDate).getTime() || new Date().getTime();
+            job.duration = job.endingDate - job.startingDate;
+            totalDuration += job.duration;
+        }
+
+        let x = 0;
+        for (let i = 0; i < jobs.length; i++) {
+            const job = jobs[i];
+            const width =
+                (job.duration / totalDuration) *
+                (WIDTH - SPACING * (jobs.length - 1));
+            draw.rect(width, HEIGHT)
+                .radius(10)
+                .move(x, EXTERIOR_SPACING)
+                .addClass("timeline-bar")
+                .addClass(job.isInternship ? "is-internship" : "is-job")
+                .on("mouseenter", () => {
+                        selectJob(job);
+                    })
+                    .click((e) => {
+                        selectJob(job);
+                    });
+
+            x += width + SPACING / 2;
+            draw.circle(HEIGHT)
+                .center(x, EXTERIOR_SPACING + HEIGHT / 2)
+                .addClass("is-background");
+            x += SPACING / 2;
+        }
+    });
+
+    const WIDTH = 500;
+    const HEIGHT = 20;
+    const SPACING = 30;
+    const EXTERIOR_SPACING = 10;
+
+    let svg = null;
+    $: selectedJob = jobs[jobs.length - 1];
     export let jobs = [];
 </script>
 
+<h2 class="giant-title is-overlapping fil-ariane-balise" id="title-where">
+    Where?
+</h2>
 
-<h2 class="giant-title is-overlapping fil-ariane-balise" id="title-where">Where?</h2>
-
-<div class="grid timeline">
-    {#each months as month}
-        <a
-            class="timeline-container"
-            on:mouseenter={() => selectJob(month.job)}
-            on:click={() => selectJob(month.job)}
-            on:keydown={() => selectJob(month.job)}
-            on:keyup={() => selectJob(month.job)}
-            on:keypress={() => selectJob(month.job)}
-        >
-            <span
-                class="timeline-bar"
-                class:is-internship={month.isIntenship}
-                class:is-job={month.isJob}
-                class:first-or-last={month.isFirstOrLast}
-            />
-            {#if month.month == 1}
-                <span class="year">{month.year}</span>
-            {/if}
-        </a>
-    {/each}
+<div class="grid">
+    <div class="col is-6 on-sm-is-12">
+    <svg class="timeline" bind:this={svg} width="100%"></svg>
+    </div>
 </div>
 
 <div class="grid">
     <div class="col is-6 on-sm-is-12">
         {#if selectedJob}
             <Card>
-                <span slot="title" class="job-header">
+                <div slot="title" class="is-text-centered">
                     <h3>{selectedJob.company}</h3>
                     <b>
                         {selectedJob.location}
@@ -108,7 +87,7 @@
                         {formatDate(selectedJob.startingDate)} -
                         {formatDate(selectedJob.endingDate)}
                     </p>
-                </span>
+                </div>
                 <p class="description">{@html selectedJob.description}</p>
             </Card>
         {/if}
@@ -116,45 +95,25 @@
 </div>
 
 <style>
-    .timeline {
-        align-items: center;
-        flex-wrap: nowrap;
-        margin: 2rem 0;
+    :global(.timeline .is-background) {
+        fill: var(--color-background-more);
     }
 
-    .timeline-container {
+    :global(.timeline .timeline-bar) {
+        transform-origin: center;
         transition: 0.3s;
-        position: relative;
-        padding: 0 0.1rem;
-    }
-    .timeline-container .year {
-        font-family: "Teko", sans-serif;
-        position: absolute;
-        color: var(--color-midground);
+        cursor: pointer;
     }
 
-    .timeline-bar {
-        display: block;
-        margin: 0.5rem 0;
-        width: 0.3rem;
-        height: 1rem;
-        border-radius: 0.4rem;
-        background-color: var(--color-midground);
-    }
-    .timeline-bar.is-internship {
-        background-color: var(--color-primary-less);
-        height: 1.5rem;
-        margin: 0.25rem 0;
-    }
-    .timeline-bar.is-job {
-        background-color: var(--color-primary);
-        height: 2rem;
-        margin: 0;
-    }
-    .timeline-bar.first-or-last {
-        height: 1.5rem;
-    }
-    .timeline-container:hover {
+    :global(.timeline .timeline-bar:hover) {
         transform: scaleY(1.2);
+    }
+
+    :global(.timeline .timeline-bar.is-internship) {
+        fill: var(--color-primary-low);
+    }
+
+    :global(.timeline .timeline-bar.is-job) {
+        fill: var(--color-primary);
     }
 </style>
