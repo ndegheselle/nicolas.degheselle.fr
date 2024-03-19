48f73d879d200c1ed5b6511ffa9d99c22c3ea205
Nicolas de gheselle <nicolas@degheselle.fr>
2024-03-19 20:14:17 +0100

    Update skills.json and CV layout

--- src/pages/cv/CV.svelte
+++ src/pages/cv/CV.svelte
@@ -7,8 +7,9 @@
 
 <header class="container">
     <div class="grid is-auto">
-        <div class="col">
+        <div class="col main-header is-8">
             <h1>Nicolas DE GHESELLE</h1>
+            <p>{me.description}</p>
         </div>
         <span class="col contacts">
             <a href="mailto:nicolas@degheselle.fr">nicolas@degheselle.fr</a>
@@ -16,31 +17,28 @@
             <span>+33 7 68 18 51 80</span>
         </span>
     </div>
-    <p>{me.description}</p>
 </header>
 
 <main class="container">
-
     <div class="grid is-auto">
         <h2 class="col is-auto">Expérience professionnelle</h2>
         <span class="col is-separator"></span>
     </div>
 
     <section class="part">
-        {#each jobs as job }
-        <div class="grid is-auto">
-            <div class="col is-1 on-md-is-12">
-                <span class="subtitle">
-                    {formatDates(job.startingDate, job.endingDate)}
-                </span>
+        {#each jobs as job}
+            <div class="grid is-auto">
+                <div class="col is-1 on-md-is-12">
+                    <span class="subtitle">
+                        {formatDates(job.startingDate, job.endingDate)}
+                    </span>
+                </div>
+                <div class="col is-content">
+                    <h3>{job.title}</h3>
+                    <span class="subtitle">{job.company} - {job.location}</span>
+                    <p>{@html job.shortDescription}</p>
+                </div>
             </div>
-            <div class="col is-content">
-                <h3>{job.title}</h3>
-                <span class="subtitle">{job.company} - {job.location}</span>
-                <p>{@html job.shortDescription}</p>
-            </div>
-        </div>
-
         {/each}
     </section>
 
@@ -50,17 +48,17 @@
     </div>
 
     {#each me.educations as education}
-    <div class="grid is-auto">
-        <div class="col is-1 on-md-is-12">
-            <span class="subtitle">
-                {formatDate(education.date)}
-            </span>
+        <div class="grid is-auto">
+            <div class="col is-1 on-md-is-12">
+                <span class="subtitle">
+                    {formatDate(education.date)}
+                </span>
+            </div>
+            <div class="col is-content">
+                <h3>{education.name}</h3>
+                <span class="subtitle">{@html education.description}</span>
+            </div>
         </div>
-        <div class="col is-content">
-            <h3>{education.name}</h3>
-            <span class="subtitle">{@html education.description}</span>
-        </div>
-    </div>
     {/each}
 
     <div class="grid is-auto">
@@ -73,26 +71,49 @@
         <span class="col is-separator"></span>
     </div>
     {#each me.certifications as certification}
-    <div class="grid is-auto">
-        <div class="col is-1 on-md-is-12">
-            <span class="subtitle">
-                {formatDate(certification.date)}
-            </span>
+        <div class="grid is-auto">
+            <div class="col is-1 on-md-is-12">
+                <span class="subtitle">
+                    {formatDate(certification.date)}
+                </span>
+            </div>
+            <div class="col is-content">
+                <h3>{certification.name}</h3>
+                <span class="subtitle">{@html certification.description}</span>
+            </div>
         </div>
-        <div class="col is-content">
-            <h3>{certification.name}</h3>
-            <span class="subtitle">{@html certification.description}</span>
-        </div>
-    </div>
     {/each}
 </main>
 
 <style>
+
+@media print {
+    :global(html)
+    {
+    font-size: 16px;
+    }
+}
+
+
     header {
         background: var(--color-background-more-2);
+        padding: 1rem;
     }
 
-    h1, h2, h3, p {
+    .main-header {
+        border-bottom: 0.2rem var(--color-primary-less) solid;
+        border-top: 0.2rem var(--color-primary-less) solid;
+        padding: 1rem 0;
+    }
+
+    .container {
+        padding: 1rem;
+    }
+
+    h1,
+    h2,
+    h3,
+    p {
         margin: 0;
     }
 
@@ -119,5 +140,4 @@
         border-top: 0.1rem solid var(--color-background-more-1);
         margin-left: 1rem;
     }
-
 </style>
